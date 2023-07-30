8a04c3513b2f8093eb785c2ad6e8ef43a4922ca1
Manpreet Chahal <46927185+Itsmanrpchahal@users.noreply.github.com>
2023-07-30 13:26:43 +0530

    Update

--- App.tsx
+++ App.tsx
@@ -12,33 +12,21 @@ import RN_Speedo_Meter from './src/RN_Speedo_meter';
 
 function App(): JSX.Element {
   return (
-    <View style={{width: '100%'}}>
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <RN_Speedo_Meter
         value={10}
         size={200}
         centerLabelText={true}
+        bottomLabelText={false}
         centerTextStyle={{color: 'red'}}></RN_Speedo_Meter>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
